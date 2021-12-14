b09db2c9e644eb46a8e80f12a684aa36af41e24f
郑天宇 <417002107@qq.com>
2021-12-14 10:28:52 +0800

    init

---
+++ src/components/Input.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-input v-model="propsValue.value"></el-input>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props:{
+    propsValue:{
+      type:Object,
+      default:{
+        value:'文本框'
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <component v-for="c in defaultSetting" :key="c.id" :is="c.name" :propsValue="c.propsValue" :styles="c.styles"></component>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Input from '../components/Input';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Input
+  },
+  data() {
+    return {
+      defaultSetting: [
+        {
+          name: 'Input',
+          id:'1',
+          propsValue: {
+            value: '第一个input'
+          },
+          styles:{
+            color:'red'
+          }
+
+        },
+        {
+          name: 'Input',
+          id:'2',
+          propsValue: {
+            value: '第二个input'
+          },
+          styles:{
+            color:'red'
+          }
+
+        }
+      ]
+    };
   }
-}
+};
 </script>
