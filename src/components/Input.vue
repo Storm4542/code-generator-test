0209e067fa8432873f939562f7566bf41917590d
郑天宇 <417002107@qq.com>
2021-12-14 11:20:17 +0800

    init

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,15 +1,31 @@
 <template>
-  <el-input v-model="propsValue.value"></el-input>
+  <el-input v-model="propsValue.value" :style="{...styles,...position}" style="position: absolute"></el-input>
 </template>
 
 <script>
 export default {
   name: "Input",
-  props:{
-    propsValue:{
-      type:Object,
-      default:{
-        value:'文本框'
+  props: {
+    propsValue: {
+      type: Object,
+      default: {
+        value: '文本框'
+      }
+    },
+    styles: {
+      type: Object,
+      default: {
+        width: '200px',
+        height: '80px'
+      }
+    },
+    position: {
+      type: Object,
+      default: {
+        left: '',
+        right: '',
+        top: '',
+        bottom: ''
       }
     }
   }
